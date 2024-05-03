51ddc43bcfaf6a057a0778ce9dfe45c2d68c0623
Fasih-Haider01 <fasihhaider105@gmail.com>
2024-05-03 05:22:55 -0700

    html practice codes

---
+++ class_3.js
@@ -0,0 +1,7 @@
+// anything inside of the apostrophe is called string
+// when we write let and username all that kind of stuff it is known as variable initialization
+var username = "Fasih Haider";
+var ag = 30;
+var student = true;
+//control+/ whole line gets comment out.
+// multi line comment are represented by ( /* ).
---
+++ class_3.ts
@@ -0,0 +1,7 @@
+// anything inside of the apostrophe is called string
+// when we write let and username all that kind of stuff it is known as variable initialization
+let username: string = "Fasih Haider";
+let ag: number = 30;
+let student: boolean = true;
+//control+/ whole line gets comment out.
+// multi line comment are represented by ( /* ).
